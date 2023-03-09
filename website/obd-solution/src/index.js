a6bede99effe7c09a395c806dae6240c76371e94
Nam <doanngocnam3002@gmail.com>
2023-03-09 11:56:30 +0700

    create app

---
+++ website/obd-solution/src/index.js
@@ -0,0 +1,8 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const jsxElement = <h1>OBD</h1>
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(jsxElement, rootElement)
